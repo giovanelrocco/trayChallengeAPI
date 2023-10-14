e69b419f8113747e021eae4a382fe69cd40dc8ef
Giovane Lucca Rocco <giovanelr@gmail.com>
2023-10-14 15:08:41 -0300

    [TC-13] Adicionado cron para disparo de emails

--- app/Http/Controllers/API/MailController.php
+++ app/Http/Controllers/API/MailController.php
@@ -3,31 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\VendaRepository;
-use App\Repositories\VendedorRepository;
+use App\Services\MailService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    protected $repository;
-    protected $vendedor_repository;
+    protected $service;
 
-    public function __construct(VendaRepository $repository, VendedorRepository $vendedor_repository)
+    public function __construct(MailService $service)
     {
-        $this->repository = $repository;
-        $this->vendedor_repository = $vendedor_repository;
+        $this->service = $service;
     }
 
     public function enviarAdminEmail()
     {
-        $vendas = $this->repository->listByData();
-
-        if (count($vendas) < 1) {
-            return response('Venda não encontrada', 404);
-        }
-
-        Mail::to(env('ADMIN_EMAIL', 'test@gmail.com'))->send(new \App\Mail\AdminMail($vendas));
+        $this->service->enviarEmailAdmin();
 
         return response('', 201)
             ->header('Content-Type', 'application/json');
@@ -35,15 +25,7 @@ class MailController extends Controller
 
     public function enviarLoteEmail()
     {
-        $vendedores_com_venda = $this->repository->listVendedoresByData()->toArray();
-
-        if (!$vendedores_com_venda) {
-            return response('Sem venda no dia corrente', 404);
-        }
-
-        foreach ($vendedores_com_venda as $vendedor) {
-            $this->programarEmail($vendedor['vendedor_id']);
-        }
+        $this->service->enviarLoteEmail();
 
         return response('', 201)
             ->header('Content-Type', 'application/json');
@@ -52,28 +34,9 @@ class MailController extends Controller
 
     public function enviarEmail(Request $request)
     {
-
-        $this->programarEmail($request->id);
+        $this->service->enviarEmail($request->id);
 
         return response('', 201)
             ->header('Content-Type', 'application/json');
     }
-
-    public function programarEmail(int $vendedor_id)
-    {
-        $vendas = $this->repository->listByVendedorAndData($vendedor_id);
-
-        if (count($vendas) < 1) {
-            return response('Venda não encontrada', 404);
-        }
-
-        $vendedor = $this->vendedor_repository->findById($vendedor_id);
-
-        if (!$vendedor) {
-            return response('Vendedor não encontrada', 404);
-        }
-
-        Mail::to($vendedor->email)->send(new \App\Mail\VendedorMail($vendas->toArray(), $vendedor->toArray()));
-    }
-
 }
