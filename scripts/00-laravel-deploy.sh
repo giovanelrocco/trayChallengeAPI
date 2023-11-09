44508c0d976bc1cbb08c6767d7163e43b2f0d36f
Giovane Lucca Rocco <giovanelr@gmail.com>
2023-11-09 16:21:18 -0300

    [TC-23] Teste de deploy render

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
 echo "Running composer"
+cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+echo "Clearing caches..."
+php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -14,5 +16,4 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Running seeder..."
-php artisan db:seed
+echo "done deploying"
